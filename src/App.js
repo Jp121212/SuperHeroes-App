19ba9ac926a4a40d7ea74b661a4afdc3618cd31d
Jp121212 <jose.elizondo@unadeca.net>
2022-07-06 22:14:45 -0600

    Prueba #4

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-           <code></code>Hola.
+           <code></code>Hola 1.2.3.
         </p>
         <a
           className="App-link"
