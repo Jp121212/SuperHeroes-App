b61bc977bd0e9a738c5766fd911859b101223b15
Jp121212 <jose.elizondo@unadeca.net>
2022-07-06 22:23:36 -0600

    Prueba #5

--- src/App.js
+++ src/App.js
@@ -3,22 +3,7 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-           <code></code>Hola 1.2.3.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <p>Hola esto es una prueba de cambio :d</p>
   );
 }
 
