1b5984a9dfd1222815b306c3ecf27a8c469895e5
Jp121212 <jose.elizondo@unadeca.net>
2022-07-09 03:53:16 -0600

    Metodo eliminar items
    Este fue el metodo mas viable que encontre para elminar mis elemntos de la forma en la que lo hice

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,16 @@ import './App.css';
 import React, { useEffect, useState } from "react";
 import Data from "./Data";
 import { BrowserRouter as Router } from "react-router-dom";
-import { Button } from '@mui/material';
+import { Button,Avatar } from '@mui/material';
+import { width } from '@mui/system';
 
 var persjes = JSON.parse(localStorage.getItem('data') || "[]");
 var id = Math.floor(Math.random()*733)
 function App(){
 const {data, loading, error,randompers} = Data(`https://pokeapi.co/api/v2/pokemon/${id}`);
 const heartblack ="🖤";
+const [saveData,setSaveData] = useState(false);
+const [saveData2,setSaveData2] = useState(true);
 
   
 if (loading) return <h3>Cargando...</h3>;
@@ -17,15 +20,25 @@ if (loading) return <h3>Cargando...</h3>;
 if(error) console.log(error);
 
 const add = ()=> {
-    
-      
   var pers={
       id: data.id,
       name: data.name
   }
   persjes.push(pers);
   localStorage.setItem('data',JSON.stringify(persjes));
+  setSaveData(true);
   console.log(persjes);
+}
+ 
+
+const deleteObj = (id)=>{
+  
+  const filteredPers = persjes.filter(pers => pers.id !== id);
+  localStorage.setItem('data',JSON.stringify(filteredPers));
+  setSaveData(false);
+  window.location.reload();
+  console.log(filteredPers);
+  
 }
 console.log(data)
 
@@ -36,7 +49,7 @@ return(
     
     <button onClick={() => window.location.reload()}>Random</button>
     <button onClick={add}> {heartblack}</button>   
-    <button onClick={randompers}> refresh </button>
+   
     
   <div>
    
@@ -51,7 +64,8 @@ return(
           persjes.map(pers =>(
           <tr key={pers.name}>
               <td>
-              <span>🦸‍♂️</span>{pers.name}<Button>eliminar</Button>
+              <span>🦸‍♂️</span>{pers.name}<Button onClick={() => deleteObj(pers.id)}
+              startIcon={<Avatar src={'https://cdn-icons.flaticon.com/png/512/3976/premium/3976961.png?token=exp=1657356051~hmac=7db6e9405515eacd18d44f37189fe8bf'}/>}></Button>
               </td>
           </tr> 
           ))
